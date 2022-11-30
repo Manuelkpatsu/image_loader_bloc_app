2634949908a88459ea9960c414579b5f09375277
Emmanuel Ahuno <manuelkpatsu@gmail.com>
2022-11-30 18:59:34 +0000

    Add test

--- lib/bloc/app_bloc.dart
+++ lib/bloc/app_bloc.dart
@@ -6,6 +6,7 @@ import 'app_state.dart';
 import 'bloc_events.dart';
 
 typedef AppBlocRandomUrlPicker = String Function(Iterable<String> allUrls);
+typedef AppBlocUrlLoader = Future<Uint8List> Function(String url);
 
 extension RandomElement<T> on Iterable<T> {
   T getRandomElement() => elementAt(math.Random().nextInt(length));
@@ -14,25 +15,31 @@ extension RandomElement<T> on Iterable<T> {
 class AppBloc extends Bloc<AppEvent, AppState> {
   String _pickRandomUrl(Iterable<String> allUrls) => allUrls.getRandomElement();
 
+  Future<Uint8List> _loadUrl(String url) => NetworkAssetBundle(Uri.parse(url))
+      .load(url)
+      .then((byteData) => byteData.buffer.asUint8List());
+
   AppBloc({
     required Iterable<String> urls,
     Duration? waitBeforeLoading,
     AppBlocRandomUrlPicker? urlPicker,
+    AppBlocUrlLoader? urlLoader,
   }) : super(const AppState.empty()) {
-    on<LoadNextUrlEvent>((event, emit) async {
-      // start loading
-      emit(const AppState(isLoading: true, data: null, error: null));
-      final url = (urlPicker ?? _pickRandomUrl)(urls);
-      try {
-        if (waitBeforeLoading != null) {
-          await Future.delayed(waitBeforeLoading);
+    on<LoadNextUrlEvent>(
+      (event, emit) async {
+        // start loading
+        emit(const AppState(isLoading: true, data: null, error: null));
+        final url = (urlPicker ?? _pickRandomUrl)(urls);
+        try {
+          if (waitBeforeLoading != null) {
+            await Future.delayed(waitBeforeLoading);
+          }
+          final data = await (urlLoader ?? _loadUrl)(url);
+          emit(AppState(isLoading: false, data: data, error: null));
+        } catch (e) {
+          emit(AppState(isLoading: false, data: null, error: e));
         }
-        final bundle = NetworkAssetBundle(Uri.parse(url));
-        final data = (await bundle.load(url)).buffer.asUint8List();
-        emit(AppState(isLoading: false, data: data, error: null));
-      } catch (e) {
-        emit(AppState(isLoading: false, data: null, error: e));
-      }
-    });
+      },
+    );
   }
 }
